ec0513fe851fc681eb9d8140e5fc0b1092f55a44
Kristofer_Almeros <kristofer.almeros@hotmail.com>
2024-10-24 12:34:26 +0200

    responses

--- Backend/utils/responses.js
+++ Backend/utils/responses.js
@@ -4,8 +4,8 @@ export const sendResponse = (code, message) => {
         statusCode: code,
         headers: {
             "Access-Control-Allow-Origin": "http://lckd-password-generator-bucket-prod.s3-website.eu-north-1.amazonaws.com",
-            "Access-Control-Allow-Headers": "Content-Type",  // Allows specific headers
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",  // Specifies allowed HTTP methods
+            "Access-Control-Allow-Headers": "Content-Type",  
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE", 
         },
         body: JSON.stringify(message)
     }
@@ -15,7 +15,7 @@ export const sendError = (code, message) => {
     return {
         statusCode: code,
         headers: {
-            "Access-Control-Allow-Origin": "*",  // Allows all domains
+            "Access-Control-Allow-Origin": "http://lckd-password-generator-bucket-prod.s3-website.eu-north-1.amazonaws.com",  
             "Access-Control-Allow-Headers": "Content-Type",
             "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
         },
