bef2c03a8ffc250ccd852d27379ae1dcfcf580ba
Kristofer_Almeros <kristofer.almeros@hotmail.com>
2024-10-27 21:07:16 +0100

    newlckd page

--- Frontend/src/pages/NewLCKDPage/NewLCKDPage.jsx
+++ Frontend/src/pages/NewLCKDPage/NewLCKDPage.jsx
@@ -1,11 +1,34 @@
 import "./NewLCKDPage.css"
 import axios from 'axios';
 import { useForm } from "react-hook-form";
+import generator from 'generate-password'; 
+import { GeneratePassword } from "js-generate-password";
+import genPasImg from "../../assets/genPasImg.svg"
+
 
 
 import logo from "../../assets/logo.svg"
 
 export const NewLCKDPage = () => {
+    const { register, handleSubmit, setValue, formState: { errors }} = useForm()
+
+    const generatePassword = () => {
+        const newPassword = GeneratePassword({
+            length: 10,
+            symbols:true
+        })
+        setValue("securePassword", newPassword)
+    }
+    // const password = GeneratePassword({
+    //     length: 10,
+    //     symbols: true
+    // })
+    // console.log("Password", password)
+
+    const handleSubmitCredentials = (data) => {
+        console.log(data)
+    }
+
     return (
         <div className="newLCKDPage-container">
             <header>
@@ -14,6 +37,53 @@ export const NewLCKDPage = () => {
             </header>
 
             <h1 className="title-text">NEW SECURE CREDENTIALS</h1>
+
+            <form onSubmit={handleSubmit(handleSubmitCredentials)}>
+                <div className="form-group">
+                    <label className="label-text">www</label>
+                    <input 
+                        {...register("www", {required: "Adress is required"})}
+                        className="input-field"
+                    />
+                    <small className="text-danger">
+                        {errors.www && errors.www.message}
+                    </small>
+                </div>
+
+                <div className="form-group">
+                    <label className="label-text">username</label>
+                    <input
+                        {...register("username", {required: "Username is required"})}
+                        className="input-field"
+                    />
+                    <small className="text-danger">
+                        {errors?.username && errors.username.message}
+                    </small>
+                </div>
+
+                <div className="form-group" style={{ position: "relative" }}>
+                    <label className="label-text">secure password</label>
+                    <input 
+                        {...register("securePassword", {required: "Passoword is required"})}
+                        className="input-field genPassword"
+                        defaultValue="" 
+                    />
+                    <img 
+                        src={genPasImg}
+                        alt="Generate password"
+                        onClick={generatePassword}
+                        className="generatePassword-image"
+                    />
+            
+
+                    <small>
+                        {errors?.securepassword && errors.securepassword.message}
+                    </small>
+                </div>
+
+                <button className="submit-button">CREATE LCKD</button>
+            </form>
+    
 
 
         </div>
